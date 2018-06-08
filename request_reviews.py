0977f713422d1df17772c647f6e328525ddb27f5
ethan1022 <ethan1022@gmail.com>
2018-06-08 11:42:50 +0800

    add another condition for more security

--- request_reviews.py
+++ request_reviews.py
@@ -121,15 +121,17 @@ class app_reviews_scanner(object):
 				else:
 					if content != current_id:
 						new_review_index_array = []
+						founded = False
 						for index,element in enumerate(array):
 							if index != 0:
 								perDic = self.parsing_one_data(element)
 								if content == perDic["identifier"]:
+									founded = True
 									break
 								else:
 									new_review_index_array.append(array.index(element))
 
-						if len(new_review_index_array) != len(array) - 1:
+						if len(new_review_index_array) != len(array) - 1 and founded == True:
 							for element in new_review_index_array:
 								new_review_dic = self.parsing_data(array, element)
 								self.send_slack_message(new_review_dic, which_country)
